e049c8bfc716e9c187badaef51d5f8f6d773abf3
Mark <markbrowncms@gmail.com>
2019-09-29 01:57:52 -0700

    Updates

---
+++ precache-manifest.e311dd90444f90939e1cac8fda226b00.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6b194016eaf3d5d79a1fcf83aeadbe3b",
+    "url": "/brownreact-app/index.html"
+  },
+  {
+    "revision": "e0952cd26d00148d8061",
+    "url": "/brownreact-app/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "24465b6e038cf0578bb1",
+    "url": "/brownreact-app/static/js/2.8a934b75.chunk.js"
+  },
+  {
+    "revision": "e0952cd26d00148d8061",
+    "url": "/brownreact-app/static/js/main.68c8d3f3.chunk.js"
+  },
+  {
+    "revision": "eb538306ec277d5765b4",
+    "url": "/brownreact-app/static/js/runtime-main.3ece75cc.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/brownreact-app/static/media/logo.25bf045c.svg"
+  }
+]);
